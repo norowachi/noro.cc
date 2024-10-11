7c2ac8575ca4b53b8c215581e7e97eb4c18cf1d7
Andrew <contact@norowa.dev>
2024-10-11 20:45:33 +0200

    perma redirect in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,18 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  redirects: {
+    "/discord": {
+      status: 308,
+      destination: "https://discord.gg/fqJmXyEpEj",
+    },
+    "/gitbot/invite": {
+      status: 308,
+      destination: "https://discord.com/oauth2/authorize?client_id=695588314765459488",
+    },
+    "/gitbot/repo": {
+      status: 308,
+      destination: "https://github.com/norowachi/gitbot",
+    },
+  },
 });
--- src/pages/gitbot/repo.ts
+++
@@ -1,6 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = ({ redirect }) => {
-  // 308 Permanent Redirect - with same method
-  return redirect("https://github.com/norowachi/gitbot", 308);
-};
