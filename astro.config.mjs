38e28d5b2808cfeb466d2ce2eb58d0c67d21ada6
Andrew <contact@norowa.dev>
2024-10-11 20:56:19 +0200

    test different routing method

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,8 @@ import icon from "astro-icon";
 export default defineConfig({
   integrations: [tailwind(), icon()],
   redirects: {
-    "/discord": {
-      status: 308,
-      destination: "https://discord.gg/fqJmXyEpEj",
-    },
-    "/gitbot/invite": {
-      status: 308,
-      destination: "https://discord.com/oauth2/authorize?client_id=695588314765459488",
-    },
-    "/gitbot/repo": {
-      status: 308,
-      destination: "https://github.com/norowachi/gitbot",
-    },
+    "/discord": "https://discord.gg/fqJmXyEpEj",
+    "/gitbot/invite": "https://discord.com/oauth2/authorize?client_id=695588314765459488",
+    "/gitbot/repo": "https://github.com/norowachi/gitbot",
   },
 });
