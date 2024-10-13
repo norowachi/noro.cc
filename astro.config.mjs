c67e07c3b130f8ac0a09429ec315ebb4c8336408
Andrew <contact@norowa.dev>
2024-10-14 00:23:31 +0200

    removed useless wrapping and added new redirection stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,4 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  redirects: {
-    "/discord": "https://discord.gg/fqJmXyEpEj",
-    "/gitbot/invite": "https://discord.com/oauth2/authorize?client_id=695588314765459488",
-    "/gitbot/repo": "https://github.com/norowachi/gitbot",
-  },
 });
