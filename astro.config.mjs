b32ae1d9c9a04c8d9e74e20394d9634fa29edb01
norowachi <contact@noro.cc>
2025-05-31 03:17:28 +0200

    updates & some QOL fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon(), svelte()],
-	adapter: vercel(),
+  integrations: [icon(), svelte()],
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
