41c65b6d799b68feb9a5b265036f9306ca7e0bd9
Andrew <contact@noro.cc>
2025-10-06 11:27:29 +0200

    remove unused env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 
-interface ImportMetaEnv {
-	readonly DISCORD_CLIENT_ID: string;
-	readonly DISCORD_CLIENT_SECRET: string;
-	readonly DISCORD_APPEALS_WEBHOOK: string;
-	readonly ENCRIPTION_KEY: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
